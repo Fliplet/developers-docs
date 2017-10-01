7674b0e2989828589a0c3d5ef6ac862cbc72b6aa
Nicholas Valbusa <squallstar@gmail.com>
2017-10-01 16:06:53 +0200

    search results page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Fliplet' });
+  res.render('index');
+});
+
+router.get('/search', function (req, res) {
+  res.render('search-results', {
+    query: req.query.q
+  });
 });
 
 module.exports = router;
