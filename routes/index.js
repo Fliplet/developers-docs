bffa012221ed4814d1f204dc42e1c24f84448116
Nicholas Valbusa <squallstar@gmail.com>
2017-10-02 01:09:52 +0100

    new content

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
+const textSearch = require('rx-text-search');
 const express = require('express');
+const _ = require('lodash');
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -6,9 +8,39 @@ router.get('/', function(req, res) {
 });
 
 router.get('/search', function (req, res) {
-  res.render('search-results', {
-    query: req.query.q
-  });
+  const query = req.query.q;
+
+  textSearch.findAsPromise(query, '**/*.pug', {cwd: 'views/docs'})
+    .then(function (results) {
+        results = _.uniqBy(results, 'file');
+
+        results.forEach(function (page) {
+          page.uri = '/' + page.file.replace(/\.pug$/, '');
+
+          res.locals.menu.some(function (section) {
+            const menuResult = _.find(section.pages, function (p) {
+              return p.uri === page.uri;
+            });
+
+            if (menuResult) {
+              page.menu = menuResult;
+              page.section = section.label;
+              return true;
+            }
+          });
+        });
+
+        results = _.filter(results, function (result) {
+          return !!result.menu;
+        });
+
+        res.render('search-results', {
+          query,
+          results
+        });
+      }).catch(function (err) {
+        res.send(err);
+      });
 });
 
 module.exports = router;
